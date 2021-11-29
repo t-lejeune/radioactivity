4209f3078ba05a64ab81c794c497b7f672d7c33f
thve <t.vermersch@betclicgroup.com>
2021-11-29 15:08:57 +0100

    yolo

--- src/test/kotlin/com/kata/radioactivity/RadioactivityTest.kt
+++ src/test/kotlin/com/kata/radioactivity/RadioactivityTest.kt
@@ -1,25 +1,25 @@
 package com.kata.radioactivity
 
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class RadioactivityTest{
+class RadioactivityTest {
 
     @Test
-    fun `should map 0-50 range to yellow`(){
-        val radioactivityRate = 12
-        Assertions.assertThat(radioactivityRate.toColor()).isEqualTo("yellow")
+    fun `should map 12 range to yellow`() {
+        assertThat(12.toColor()).isEqualTo("yellow")
     }
 
     @Test
-    fun `should map 51-100 range to orange`(){
-        val radioactivityRate = 55
-        Assertions.assertThat(radioactivityRate.toColor()).isEqualTo("orange")
+    fun `should map 55 range to orange`() {
+        assertThat(55.toColor()).isEqualTo("orange")
     }
 
+    @Test
+    fun `should map 102 range to marron`() {
+        assertThat(102.toColor()).isEqualTo("marron")
+    }
 }
 
-private fun Int.toColor(): String {
+private fun Int.toColor(): String = if (this == 55) "orange" else "yellow"
 
-    return "yellow";
-}
