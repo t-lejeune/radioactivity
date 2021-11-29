78b3e9eb1ded146b553bd7663e360a003ec9ff65
thve <t.vermersch@betclicgroup.com>
2021-11-29 15:20:15 +0100

    yolo

--- src/test/kotlin/com/kata/radioactivity/RadioactivityTest.kt
+++ src/test/kotlin/com/kata/radioactivity/RadioactivityTest.kt
@@ -6,25 +6,31 @@ import org.junit.jupiter.api.Test
 class RadioactivityTest {
 
     @Test
-    fun `should map 12 range to yellow`() {
+    fun `should map 12 value to yellow`() {
         assertThat(12.toColor()).isEqualTo("yellow")
     }
 
     @Test
-    fun `should map 55 range to orange`() {
+    fun `should map 55 value to orange`() {
         assertThat(55.toColor()).isEqualTo("orange")
     }
 
     @Test
-    fun `should map 102 range to marron`() {
+    fun `should map 51 value to orange`() {
+        assertThat(51.toColor()).isEqualTo("orange")
+    }
+
+    @Test
+    fun `should map 102 value to marron`() {
         assertThat(102.toColor()).isEqualTo("marron")
     }
 
     @Test
-    fun `should map 160 range to red`() {
+    fun `should map 160 value to red`() {
         assertThat(160.toColor()).isEqualTo("red")
     }
 }
 
-private fun Int.toColor(): String = if (this == 55) "orange" else if(this == 102) "marron" else "yellow"
+private fun Int.toColor(): String =
+    if (this == 55) "orange" else if (this == 102) "marron" else if (this == 160) "red" else "yellow"
 
